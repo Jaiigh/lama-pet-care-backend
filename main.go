8914ffb286d58eff035f5b87af671fde21e5417f
Auuming <148842078+Auuming@users.noreply.github.com>
2025-09-17 01:33:51 +0700

    change host for deploy test

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 // @title LAMA Backend API
 // @version 1.0
 // @description this is a backend REST API server for LAMA project
-// @host lama-pet-care-backend-qbwz.onrender.com
+// @host lama-pet-care-backend-ik0k.onrender.com
 // @BasePath /
 // @Schemes https
 // @securityDefinitions.apikey BearerAuth
