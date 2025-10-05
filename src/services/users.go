4503b63a01f1697d2ffe8275d5e8baa7ec427373
Auuming <148842078+Auuming@users.noreply.github.com>
2025-10-05 19:24:13 +0700

    mod user to update only user table

--- src/services/users.go
+++ src/services/users.go
@@ -1,7 +1,6 @@
 package services
 
 import (
-	"fmt"
 	"lama-backend/domain/entities"
 	"lama-backend/domain/repositories"
 )
@@ -40,39 +39,5 @@ func (s *UsersService) DeleteUsersByID(id string) (*entities.UserDataModel, erro
 
 func (s *UsersService) UpdateUsersByID(id string, data entities.UpdateUserModel) (*entities.UserDataModel, error) {
 	// You could add validation, logging, or pre-checks here
-	updateUserData, err := s.UsersRepository.UpdateByID(id, data)
-	if err != nil {
-		return nil, err
-	}
-
-	var roleData *entities.UserDataModel
-	switch updateUserData.Role {
-	case "doctor":
-		roleData, err = s.DoctorRepository.UpdateByID(id, data)
-		if err != nil {
-			return nil, err
-		}
-	case "caretaker":
-		roleData, err = s.CaretakerRepository.UpdateByID(id, data)
-		if err != nil {
-			return nil, err
-		}
-	case "owner":
-		roleData, err = s.OwnerRepository.UpdateByID(id, data)
-		if err != nil {
-			return nil, err
-		}
-	default:
-		return nil, fmt.Errorf("record doesn't have role")
-	}
-
-	updateUserData.LicenseNumber = roleData.LicenseNumber
-	updateUserData.StartDate = roleData.StartDate
-	updateUserData.StartWorkTime = roleData.StartWorkTime
-	updateUserData.EndWorkTime = roleData.EndWorkTime
-	updateUserData.Specialization = roleData.Specialization
-	updateUserData.Rating = roleData.Rating
-	updateUserData.TotalSpending = roleData.TotalSpending
-
-	return updateUserData, nil
+	return s.UsersRepository.UpdateByID(id, data)
 }
