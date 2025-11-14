8612c31fd0b27825c8e10145e7ea85a20bcaf318
Techit Pramanun <156179907+JaChet92@users.noreply.github.com>
2025-11-15 04:24:55 +0700

    getallU for admin

--- src/gateways/v1/route.go
+++ src/gateways/v1/route.go
@@ -25,6 +25,9 @@ func GatewayUsers(gateway HTTPGateway, app *fiber.App) {
 	user.Patch("/profile", gateway.UpdateUserPicture)
 	user.Delete("/", gateway.DeleteUserByID)
 
+	admin := api.Group("/admin", middlewares.SetJWtHeaderHandler())
+	admin.Get("/users", gateway.GetAllUsers)
+
 	services := api.Group("/services", middlewares.SetJWtHeaderHandler())
 	services.Post("/", gateway.CreateService)
 	services.Get("/", gateway.GetMyServices)
